7ed701957cf9b9896367862565e0ef314b4957a7
Joel M <hj923@hotmail.com>
2019-05-04 17:52:27 +0900

    Update target-number(not-working).py

--- problems/target-number(not-working).py
+++ problems/target-number(not-working).py
@@ -1,13 +1,7 @@
 # https://programmers.co.kr/learn/courses/30/lessons/43165?language=python3
-def bfs(total, numbers : int, target : int):
-    if total == target: # found
-        print('found')
-        return 1
-    if total > target or (total < target and not numbers): # not found. Halt searching.
-        print('not found')
-        return 0    
-    num = numbers.pop()
-    bfs(-num+total, numbers, target)
-    bfs(num+total, numbers, target)
-
-print(bfs(0, [1,1,1,1,1], 3)) # should be 5
+def dfs(total : int, numbers : int, target : int):
+    if numbers:
+        num = numbers.pop(0) # this part does not work properly. Have to use array concatenation
+        return dfs(-num+total, numbers, target) + dfs(num+total, numbers, target)
+    else:
+        return 1 if total == target else 0

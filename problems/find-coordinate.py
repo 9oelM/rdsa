6e7cd02790092707eb31f756432af40d0346e1dc
Joel M <hj923@hotmail.com>
2019-05-11 09:57:31 +0900

    Create find-coordinate.py

---
+++ problems/find-coordinate.py
@@ -0,0 +1,12 @@
+# https://programmers.co.kr/tryouts/3929/challenges/11616
+from typing import List
+
+findHypotenuseVertices = lambda v : [v1 for i, v1 in enumerate(v) for v2 in v[:i] + v[i:] if v1[0] != v2[0] and v1[1] != v2[1] ]
+findMidPoint = 
+def solution(v):
+    hypotenuseVertices = findHypo
+    print(hypotenuseVertices)
+    
+    answer = []
+
+    return answer

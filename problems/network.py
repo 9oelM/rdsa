2d0dc7126183a1c4d140f9ce6328f852a719bf6a
Joel M <hj923@hotmail.com>
2019-07-21 15:16:28 +0900

    Update network.py

--- problems/network.py
+++ problems/network.py
@@ -1,12 +1,9 @@
 # https://programmers.co.kr/learn/courses/30/lessons/43162
 
 def findPath(startNode, graph):
-
     visited = []
     stack = []
-
     stack.append(startNode)
-
     while stack:
         # get one of the connections from this node
         node = stack.pop()
@@ -15,23 +12,25 @@ def findPath(startNode, graph):
             visited.append(node)
             # then, move to the visited node and start over
             stack += graph[node]
-    
     return visited
 
-
-def solution(n, computers):    
-    nodes = {idx : [] for idx in range(n)}
-    for i, computer in enumerate(computers):
-        for idx, elem in enumerate(computer):
-            if elem == 1 and i != idx:
-                nodes[i].append(idx)
-
-    paths = []    
-    for node in nodes:
-        paths += [findPath(node, nodes)]
+def solution(n : int, computers : List[List[int]]) -> int:
+    """
+    1. make a dictionary with keys specifying the node number and values specifying the list of the node numbers it's connected to
+    2. traverse from each node to check path
+    3. unique paths 
+    """
+    nodes = {idx: [] for idx in range(n)}
+    for idx, computer in enumerate(computers):
+        for idx2, node in enumerate(computer):
+            if node == 1 and idx != idx2:
+                nodes[idx2].append(idx)
+    paths = []
+    for key in nodes:
+        paths += [searchPath(key, nodes)]
     paths = [sorted(path) for path in paths]
-    # unique paths
     paths = list({tuple(path) for path in paths})
+    
     return len(paths)
 
 testCases = [(3, [[1, 1, 0], [1, 1, 0], [0, 0, 1]]), (3, [[1, 1, 0], [1, 1, 1], [0, 1, 1]])]

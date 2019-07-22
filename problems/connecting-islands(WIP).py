d2dd56cb34aa4b07c7c680e5565d80e90ba75c67
Joel M <hj923@hotmail.com>
2019-07-22 13:58:15 +0900

    Update connecting-islands(WIP).py

--- problems/connecting-islands(WIP).py
+++ problems/connecting-islands(WIP).py
@@ -1,33 +1,32 @@
-# https://programmers.co.kr/learn/courses/30/lessons/42861
 import sys
+from typing import List
 
-def solution(n, costs):
+def solution(n : int, costs : List[List[int]]) -> int:
+    
     costsArray = [[0 for _ in range(n)] for __ in range(n)]
-    graph = [[] for _ in range(n)]
     for cost in costs:
         costsArray[cost[0]][cost[1]] = cost[2]
         costsArray[cost[1]][cost[0]] = cost[2]
-        graph[cost[0]] += [cost[1]]
-        graph[cost[1]] += [cost[0]]
 
-    for idx, vertex in enumerate(graph):
-        # 1. must be connected to all nodes in the graph
-        # 2. must be the smallest cost
 
-        # Try to start from all nodes
+    for islandNum in range(n):
+        print('-----')
+        nxtIdx, minCost = islandNum, 0
         visited = [False] * n
-        queue = [idx]
-        visited[idx] = True
-        minCost = sys.maxsize
-
-        while queue:
-            nxt = queue.pop(0)
-            for node in graph[nxt]:
-                if node not in visited:
-                    visited[node] = True
-                    queue.append(node)
+        visited[islandNum] = True
+        for _ in range(n-1):
+            
+            minimum = islandNum, sys.maxsize
+            for idx, elem in enumerate(costsArray[nxtIdx]):
+                if elem < minimum[1] and elem != 0 and not visited[idx]:
+                    minimum = idx, elem
                     
+            print(minimum)
+            nxtIdx = minimum[0]
+            minCost += minimum[1]
+            visited[nxtIdx] = True
+        
+    return minCost
     
-    return costsArray, graph
 
 print(solution(4, [[0,1,1],[0,2,2],[1,2,5],[1,3,1],[2,3,8]]))

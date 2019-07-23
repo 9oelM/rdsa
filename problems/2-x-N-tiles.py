da704ef4a085491f15722a33dfb7f6ced51e314d
Joel M <hj923@hotmail.com>
2019-07-23 15:21:54 +0900

    Update and rename 2-x-N-tiles(WIP).py to 2-x-N-tiles.py

---
+++ problems/2-x-N-tiles.py
@@ -0,0 +1,12 @@
+# https://programmers.co.kr/learn/courses/30/lessons/12900
+def solution(n):
+    crnt = 1
+    nxt = 2
+    if n <= 2:
+        return [crnt, nxt][n-1]
+    for _ in range(n-2):
+        nxt, crnt = crnt+nxt, nxt
+    return nxt % 1000000007
+
+print(solution(60000))
+

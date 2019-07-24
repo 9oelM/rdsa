a0149da0c75ed9584c6be5f77f53a5bcc5326b14
Joel M <hj923@hotmail.com>
2019-07-24 15:42:56 +0900

    Update overwork(WIP).py

--- problems/overwork(WIP).py
+++ problems/overwork(WIP).py
@@ -5,20 +5,12 @@ def solution(n, works):
     for _ in range(n):
         if works[-1] == 0:
             break
-        i = len(works) - 1
-        while works[i] != 0:
-            if works[i] != 0:
+        for i in reversed(range(len(works))):
+            if i - 1 != -1 and works[i] > works[i-1]: # not the first element
                 works[i] -= 1
-                break 
-            else:
-                i -= 1
-            
-        w = works[-1]
-        for j in reversed(range(len(works))):
-            if w < works[j]:
-                works = works[:j-1] + [w] + works[j-1:-1]
                 break
-    
+            elif i == 0: # the first element. This is the smallest elem
+                works[i] -= 1
     return sum([elem ** 2 for elem in works])
 
 testcases = [(4, [4, 3, 3]), (1, [2,1,2]), (3, [1,1])]

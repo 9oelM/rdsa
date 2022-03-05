c22155281e52e43cd93455120b5373495d74c609
9oelM <hj923@hotmail.com>
2022-03-05 21:08:25 +0900

    detonate most bombs

---
+++ problems/leetcode.com/detonate-maximum-bombs/detonate-maximum-bombs.py
@@ -0,0 +1,8 @@
+# https://leetcode.com/problems/detonate-the-maximum-bombs/
+
+from typing import List
+
+
+class Solution:
+    def maximumDetonation(self, bombs: List[List[int]]) -> int:
+        pass
